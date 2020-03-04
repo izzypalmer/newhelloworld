502ee28a7fe2b2471fd12088a01be12679ee22cd
izzy <isabella.palmer.2020@student.lhschools.org>
2020-03-04 12:58:48 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //bób does a flirt
+        System.out.println("Hello World!");
+        System.out.print("my name is ");
+        System.out.println("bób!");
+        System.out.println("and i must say...");
+        System.out.println("i am from a far away planet.");
+        System.out.print("Just the summer last,");
+        System.out.println(" my home was lost in a great galactic war.");
+        System.out.println("i have traveled many light years away from everything i've ever known.");
+        System.out.println("and in just this short time,");
+        System.out.println("i have seen stars collapse...");
+        System.out.println("the birth of entire solar systems...");
+        System.out.println("i have seen the truth of the galaxy...");
+        System.out.println("i have seen the fall of civilizations...");
+        System.out.println("i have seen horrors and wonders alike.");
+        System.out.print("and yet...");
+        System.out.println(" i have never seen anything as wondrous as you!");
+        System.out.println(".....");
+        System.out.println("sigh... bób gets to space travel...");
+        System.out.println("and i only went to england last summer.");
+        System.out.println("well... england has less risk of death by black hole anyway.");
+
+
+    }
+}
